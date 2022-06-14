19a236238814f676eaf2bc8ec4bc8a9319a07632
Arijit Das <arijit10das2002@gmail.com>
2022-06-14 16:06:47 +0530

    3

---
+++ src/components/UserComponent.js
@@ -0,0 +1,52 @@
+import React from "react";
+import UsersService from '../services/UserService';
+
+class UserComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            users:[]
+        }
+    }
+
+    componentDidMount(){
+        UsersService.getUsers().then((rsponse) => {
+            this.setState({users:Response.data})
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1 className="text-center">Users List</h1>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <td> User Id</td>
+                            <td> User First Name</td>
+                            <td> User Last Name</td>
+                            <td> User Email Id</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.users.map(
+                                user => 
+                                <tr key ={user.id}>
+                                    <td>{user.id}</td>
+                                    <td>{user.firstName}</td>
+                                    <td>{user.lastName}</td>
+                                    <td>{user.email}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default UserComponent;
---
+++ src/services/UserService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const USERS_REST_API_URL = "https://localhost:8080/api/users";
+
+class UsersService{
+
+    getUsers() {
+        return axios.get(USERS_REST_API_URL);
+    }
+}
+
+export default new UsersService();
